09178ebcaf6a2e97c5e60b1180a499492380eaef
Hariprasath2030 <webhari2005@gmail.com>
2025-04-02 20:38:34 +0530

    last

--- expense/middleware.ts
+++ expense/middleware.ts
@@ -1,8 +1,15 @@
-import { authMiddleware } from "@clerk/nextjs/server";
-export default authMiddleware({
-  publicRoutes: ["/sign-in", "/sign-up"],  // Ensure these routes are public
-  ignoredRoutes: ["/api/webhook(.*)"],
-});
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)'
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
@@ -11,4 +18,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
