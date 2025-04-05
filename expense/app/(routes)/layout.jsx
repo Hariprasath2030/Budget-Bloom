7abade7f6fc94b0e15783c50400e168992e9fe58
Hariprasath2030 <webhari2005@gmail.com>
2025-04-05 18:09:59 +0530

    p

--- expense/app/(routes)/layout.jsx
+++ expense/app/(routes)/layout.jsx
@@ -7,35 +7,34 @@ import { Budgets } from '@/utils/schema'
 import { eq } from 'drizzle-orm'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-function DashboardLayout({childern}) {
+function DashboardLayout({ childern }) {
 
-  const {user} = useUser();
+  const { user } = useUser();
   const router = useRouter();
   useEffect(() => {
     user && checkUserBudget();
   }, [user]);
 
-  const checkUserBudget=async ()=>
-  {
+  const checkUserBudget = async () => {
     const result = await db.select()
-    .from(Budgets)
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAdress))
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAdress))
 
     console.log(result);
-    if(result?.length === 0){
+    if (result?.length === 0) {
       router.replace('/dashboard/budgets');
     }
   }
   return (
     <div>
-        <div className='relative md:ml-64'>
-            <Dashboard
-            /> 
-        </div>
-        <div className='md:ml-64'>
-            <Dash/>
-            {childern}
-        </div>
+      <div className='relative md:ml-64'>
+        <Dashboard
+        />
+      </div>
+      <div className='md:ml-64'>
+        <Dash />
+        {childern}
+      </div>
     </div>
   )
 }
--- expense/middleware.ts
+++ expense/middleware.ts
@@ -1,17 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Define public routes
-const isPublicRoute = createRouteMatcher([
-  "/sign-in",
-  "/sign-up",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!(await auth()).userId && !isPublicRoute(req)) {
-    // Not authenticated and not on a public route
-    const signInUrl = new URL("/sign-in", req.url);
-    return Response.redirect(signInUrl);
-  }
+export default clerkMiddleware({
+  publicRoutes: ["/sign-in", "/sign-up"], // Publicly accessible routes
+  ignoredRoutes: ["/api/webhook(.*)"],   // Routes to ignore
 });
 
 export const config = {
