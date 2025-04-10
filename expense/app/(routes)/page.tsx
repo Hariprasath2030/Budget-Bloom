34a3c34597cfe468c204a1b34efdb265e2d8c711
Hariprasath2030 <webhari2005@gmail.com>
2025-04-10 23:17:56 +0530

    pjm

--- expense/app/(routes)/page.tsx
+++ expense/app/(routes)/page.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-function Home{
+function Home() {
     return (
-        <div>
-            Dashboard
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="text-2xl font-bold mb-4">
+                Dashboard
+            </div>
+            <Link href="/dashboard">
+                <button className="bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold hover:bg-blue-700 transition">
+                    Go to Dashboard
+                </button>
+            </Link>
         </div>
-    )
+    );
 }
-<Link href="/dashboard">
-    <button className="bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold hover:bg-blue-700 transition">
-        Dashboard
-    </button>
-</Link>
 
-
-export default Home
+export default Home;
