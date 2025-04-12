b4f39156b912f00b241dfcc9edc1436658a146cb
Hariprasath2030 <webhari2005@gmail.com>
2025-04-12 23:33:59 +0530

    mmm

--- expense/middleware.ts
+++ expense/middleware.ts
@@ -1,9 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware({
-
-  publicRoutes: ["/sign-in", "/sign-up"],
-  ignoredRoutes: ["/api/webhook(.*)"],
 });
 
 export const config = {
